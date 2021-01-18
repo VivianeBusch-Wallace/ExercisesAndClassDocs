7f17ca136cc99818e064169f0a147f458770c2a3
viviane <v.buschwallace@yahoo.com>
2021-01-18 18:55:31 +0100

    practiced and experimented switch

--- Jan/18-01/index.js
+++ Jan/18-01/index.js
@@ -143,3 +143,138 @@ for (i = 0; i < num.length; i++) {
   }
 }
 console.log(num);
+
+//practice - ignore below
+
+let arrayName1 = ["matt", "sara", "lara"];
+let str1 = "";
+for (let i = 0; i < arrayName1.length; i++) {
+  str1 = arrayName1[i][0].toUpperCase() + arrayName1[i].substring(1);
+  console.log(str1);
+}
+// let join = str1.join(", ");
+//why is this not working???
+
+const fromArrToStr = (str) => {
+  return str.join(", ") + ".";
+};
+console.log(fromArrToStr(["Berlin", "Paris", "Prague", "Rome"]));
+
+//switch
+
+let color = "red";
+switch (color) {
+  case "pink":
+    isLoged = false;
+
+    // more code
+    console.log("cool this is pink");
+    break;
+  case "red":
+    console.log("cool this is red");
+    break;
+  case "white":
+    console.log("cool this is white");
+    break;
+  default:
+    console.log("sorry you need to enter a color");
+}
+// more
+let userKidsNumber = 12;
+
+switch (userKidsNumber) {
+  case 1:
+    console.log("You are new parent with 1 child");
+    break;
+  case 1:
+    console.log("You are new parent with 1 child");
+    break;
+  case 2:
+    console.log("You have 2 kids nice ");
+    break;
+  case 3:
+    console.log("You have 3 kids nice ");
+    break;
+  case 4:
+  case 5:
+  case 6:
+    console.log("You have many kids nice ");
+    break;
+  default:
+    console.log("Cool you are happy parent :) ");
+}
+
+//practicing switch
+
+// exrecise: you have an online clothes store,
+// you have everything but you still need to give the buyer the option to choose the clothes in
+// different colors. Let them check if the color of the clothes they chose is still available.
+// use switch for this
+// dress colors avaibale: black, blue, beige, red
+// ex if the customer wants to buy a black dress:
+// let dress = "black"; --> Message "Great choice! We still have it in our store, order now!"
+
+let dress = "black";
+switch (dress) {
+  case "black":
+  case "blue":
+  case "beige":
+  case "red":
+    console.log("Great choice! We still have it in our store, order now!");
+    break;
+  default:
+    console.log(
+      "Sorry, we don't have this color in stock. :( Please choose another color."
+    );
+}
+
+//declare a variable assign value of Monday to it using switch return
+//if the day was matched with Monday return first day of the week.
+
+let dayOfWeek = "Monday";
+switch (dayOfWeek) {
+  case "Monday":
+    console.log("Today you have to go to work. Have a nice Monday!");
+    break;
+
+  case "Tuesday":
+    console.log(
+      "Today is a great day to start exercising! Have a great Tuesday!"
+    );
+    break;
+
+  case "Wednesday":
+    console.log("Today is a great day to be creative! Have a great Wednesday!");
+    break;
+
+  case "Thursday":
+    console.log(
+      "Today is a great day to learn a new skill! Have a great Thursday!"
+    );
+    break;
+
+  case "Friday":
+    console.log(
+      "Almost there! Today is a great day to review what you have learned! Have a great Friday!"
+    );
+    break;
+  default:
+    console.log(
+      "Great! You have nothing to do today! This means it's the weekend, yay!"
+    );
+}
+
+//Playing with combining switch with arrays
+
+let weekday = ["Monday", "Tuesday", "Wednesday"];
+let weekday1 = weekday[0];
+weekday1 = weekday[1];
+// weekday = "Wednesday"; //case "Wednesday" works
+// weekday = weekday[0]; //case weekday[0] also works
+switch (weekday1) {
+  case weekday[0]: //directly from an array, it does not work
+    console.log(`It is a weekday.`);
+    break;
+  default:
+    console.log(`It is not a day.`);
+}
