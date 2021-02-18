1fa23d8bd02109ea1d03505f44fc6b947faa0085
viviane <v.buschwallace@yahoo.com>
2021-02-18 01:06:03 +0100

    new stuff, cool finds created

--- Notes/coolFindsJS.js
+++ Notes/coolFindsJS.js
@@ -1,10 +1,11 @@
 // Cool discoveries I found on the web or somewhere else:
 
-//to check if an array has no unique values or only has same values:
+//to check if an array has no unique values/only has same values:
 const array1 = [1, 1, 1, 1];
 const firstNum = array1[0];
 console.log(array1.every((currentValue) => currentValue == firstNum));
 // expected output: true
-const array1 = [1, 2, 1, 1];
+array1 = [1, 2, 1, 1];
 const firstNum = array1[0];
 console.log(array1.every((currentValue) => currentValue == firstNum));
+// expected output: false
