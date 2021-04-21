358226e6cafa6a8681b4e1a3ee364f8f6b0059cc
viviane <v.buschwallace@yahoo.com>
2021-04-21 15:07:32 +0200

    first exercise finally done

---
+++ Apr/21-04/practiceJS.js
@@ -0,0 +1,64 @@
+// AEIOU:
+// Vowels. Create a function that takes a string in its parameters and counts the number of vowels (i.e. in English, “a, e, i, o, u”) in the string.
+// i.e. findVowels(“this is a string”) ➞ 4
+findVowels = (str) => {
+  const arr = str.split("");
+  let vowels = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+        vowels += arr[i];
+        break;
+      default:
+    }
+  }
+  let result = vowels.length;
+  return console.log(result);
+};
+findVowels("this is a string");
+findVowels("aaaeee");
+findVowels("pptttxyz");
+findVowels("Pity party");
+
+// ------------------------------------
+// SumOfNumbers.
+// Create a program that adds up the numbers in an array (of at least 3 numbers). Bonus: Print to screen both the sum and the product of these numbers.
+
+// ------------------------------------
+// No Duplicates!
+// A set is a collection of unique items. A set can be formed from an array by removing all duplicate items. Create a program which transforms an array into a set of unique values. See the examples below. Example:
+// [1, 4, 4, 7, 7, 7] ➞ [1, 4, 7]
+// [1, 6, 6, 9, 9] ➞ [1, 6, 9]
+// [2, 2, 2, 2, 2, 2] ➞ [2]
+
+// ------------------------------------
+// toCamelCase("hello_world") ➞ "helloWorld"
+// toCamelCase("javascript_is_fun") ➞ "javascriptIsFun"
+// Bonus: Write a function which accepts any amount of numbers and returns the average.
+// e.g
+// console.log(average(0)) ---> 0
+// console.log(average(1, 2)) ---> 1.5
+// console.log(average(1, 3, 6, 10)) ---> 5
+// console.log(average(12, 14, 16)) ---> 14
+
+// -----------------------------------
+// Scrabble. Write a program that, given an array of scrabble tiles, counts the maximum score that a player can earn from the tiles in their hand.
+// Example:
+// [ { tile: "N", score: 1 }, { tile: "K", score: 5 }, { tile: "Z", score: 10 }, { tile: "X", score: 8 }, { tile: "D", score: 2 }, { tile: "A", score: 1 }, { tile: "E", score: 1 } ]
+const scrabble = [
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 },
+  { tile: "E", score: 1 },
+];
+// The player's maximum score: 1 + 5 + 10 + 8 + 2 + 1 + 1 = 28
+// Counting Letters. Create a function that counts the number of occurrences of each letter in a string. Return an object with key pair values of letters and the number of occurrences for each letter.
+// Example:
+// countLetters("tree") ➞ {t: 1, r: 1, e: 2}
--- Apr/21-04/src/scripts/index.js
+++ Apr/21-04/src/scripts/index.js
@@ -27,12 +27,55 @@
 // -Native object-
 // are standard built-in objects provided by Javascript.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
-
-//Alert Popup Window
+// -----------------------------------------------
+// Alert Popup Window
 // window.alert("This is an alert message for the user.");
-//Prompt Popup Window
+// ----------------
+// Prompt Popup Window
 // let username = window.prompt(
 //   "Please enter your username",
 //   "your username here"
 // );
-//these are old! Do not use because annoying and too old method
+// these are old! Do not use because annoying and too old method
+// -----------------------------------------------
+// Execute a JS function upon click of button
+function change() {
+  window.document.getElementById("main-para");
+  console.log(
+    "This what you are selecting with the above path: ",
+    window.document.getElementById("main-para")
+  );
+  //it's better to make this a variable
+  let mainParagraph = window.document.getElementById("main-para");
+  console.log("This is your variable: ", mainParagraph);
+  // See? They are the same
+  // making a path a variable is creating a reference to the element from the path
+  window.document.getElementById("main-para").outerHTML =
+    "<h1 id='main'> Hi this is now a heading 😎</h1>";
+  console.log(mainParagraph);
+
+  window.document.getElementById("main").innerHTML =
+    "<h1 id='main'> Hi this is now a heading 😎</h1>";
+  console.log(mainParagraph);
+}
+
+// Practice the below from Hadi:
+// function change() {
+//     console.log(window.document.getElementById("main"));
+
+//     window.document.getElementById("main").outerHTML =
+//       "<h1 id='main'> Hi this is new text form Js 😎</h1>";
+//     let headerText = window.document.getElementById("main");
+//     console.log(headerText);
+//     headerText.style.backgroundColor = "orange";
+//   }
+
+//   function multiply() {
+//     let userNum = document.querySelector("#num").value;
+//     // console.log(userNum);
+//     let resultText = "";
+//     for (let i = 1; i <= 10; i++) {
+//       resultText += `${i} * ${userNum} ☘️ ${i * userNum} <br>`;
+//     }
+//     document.querySelector(".result").innerHTML = resultText;
+//   }
