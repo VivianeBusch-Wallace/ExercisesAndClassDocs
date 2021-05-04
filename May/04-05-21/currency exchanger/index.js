d2e0ef4121e231c1af1e5cb5988dd6ac66dad701
viviane <v.buschwallace@yahoo.com>
2021-05-04 12:01:21 +0200

    currency converter files created

---
+++ May/04-05-21/currency exchanger/index.js
@@ -0,0 +1,14 @@
+const year = new Date().getFullYear();
+document.querySelector(".year").innerHTML = year;
+const result = document.querySelector(".result");
+const userInput = document.querySelector("#userInput");
+const currency = document.querySelector("#currency");
+
+let userNumber = 0;
+
+function registerUserValue(e) {}
+
+function convert() {}
+
+currency.addEventListener("change", convert);
+userInput.addEventListener("keyup", registerUserValue);
--- May/04-05-21/index.js
+++ May/04-05-21/index.js
@@ -1,5 +1,9 @@
+// animation game
 const shiftAniBox = document.querySelector(".shift");
+// change color on mouseenter and mouseleave
+const changeColor = document.querySelector(".changeColor");
 
+// Start and Stop animation for the game
 function start() {
   shiftAniBox.style.animationPlayState = "running";
 }
