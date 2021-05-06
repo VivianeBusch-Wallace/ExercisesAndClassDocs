59bd541d00b68f30a613bcbe55571f595272ff6d
viviane <v.buschwallace@yahoo.com>
2021-05-06 13:45:08 +0200

    added all fetch files

--- May/06-05-21/index.js
+++ May/06-05-21/index.js
@@ -14,10 +14,12 @@ console.log("I can run first");
 
 let x = 33;
 console.log(x);
+
 // Promises
 // Promises are very similar to callbacks but they are a little bit clean on writing
-// Promises are the best solution if you want to do something that takes a long time to be done and you don't want your app to wait for it to be done, so it will work on the background, Like downloading an image or a video from another server
-
+// Promises are the best solution if you want to do something that takes a long time to be done and
+// you don't want your app to wait for it to be done, so it will work on the background,
+// Like downloading an image or a video from another server
 const checkPromise = new Promise((resolve, reject) => {
   let x = true;
   if (x) {
