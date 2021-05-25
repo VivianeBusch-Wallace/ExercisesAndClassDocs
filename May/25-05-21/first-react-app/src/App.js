0a4266d22496a2e8ab65782e25c07e418faa96c1
viviane <v.buschwallace@yahoo.com>
2021-05-25 11:22:39 +0200

    more notes

--- May/25-05-21/first-react-app/src/App.js
+++ May/25-05-21/first-react-app/src/App.js
@@ -1,11 +1,12 @@
 // all functions have to be capitalized for react to distinguish jsx components from non-react files
 function App() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is a paragraph.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -15,8 +16,26 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>This is h1</h1>
+      {console.log("Hi!")}
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter your name"></input>
+      </form>
     </div>
   );
 }
+// these look like html code but they are not
+// see how html class is actually called className here!
+// every repetitive element needs a key="", non-repetitive keys don't need a key
 
 export default App;
+// you need default, if not then use curly brackets etc.
+
+// never mix react hooks and components (functions)
+// you will always need a wrapper in your return!!!
+// if you don't want to have opening and closing tag, you can combine it into one, but all components need to be closed
+// example: <App />
+// whenever you write JS code inside the return, then you will need curly brackets!!
+// example: {console.log("Hi!")}
+
+// images should be saved in public, not in src! But it is still techically in ""./img/image.jpeg"
--- May/25-05-21/first-react-app/src/index.js
+++ May/25-05-21/first-react-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 
 import App from "./App"; //this is App.js
 
+import "./css/style.css"; // this is the css for the page
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
