64d0464ba01c757c274da35712b666d43606e7f1
viviane <v.buschwallace@yahoo.com>
2021-06-07 11:03:55 +0200

    comment exercises added

--- Jun/07-06/use-effect/src/App.js
+++ Jun/07-06/use-effect/src/App.js
@@ -3,3 +3,10 @@ function App() {
 }
 
 export default App;
+
+// Exercises >>
+// UseEffect more practice, please do every step individually
+// Stop useEffect from running on every render
+// Only Run Once, on Mount
+// Run useEffect on State Change
+// Run useEffect When a Prop Changes
