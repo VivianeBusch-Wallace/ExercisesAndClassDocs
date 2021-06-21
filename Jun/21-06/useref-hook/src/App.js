82ff34a8bb44c24bb49ed0c5918e878ff02a24d8
viviane <v.buschwallace@yahoo.com>
2021-06-21 11:26:45 +0200

    deleted files and added stuff to try out useRef

--- Jun/21-06/useref-hook/src/App.js
+++ Jun/21-06/useref-hook/src/App.js
@@ -1,24 +1,52 @@
 import React, { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const renderCount = useRef(1);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
+  // useState rerenders every time state changes
+  // useRef does not rerender every time, but it stores the information, however it does not notify anyone about it
+
+  // from Hadi
+  const [counter, setCounter] = React.useState(0); // you can use useState like this without importing it first
+  const [like, setLike] = React.useState(false);
+  const userTextInput = useRef(null);
+  const count = useRef(0);
+
+  const changeBg = () => (userTextInput.current.style.backgroundColor = "red");
+  const getMe = () => {
+    console.log(userTextInput);
+    userTextInput.current.focus();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <p>The name is {name}</p>
+      <p>Rendering {renderCount.current} times.</p>
+      {/* from Hadi */}
+      <input type="text" ref={userTextInput} />
+      <button onClick={getMe}>Get me focused</button>
+      <button onClick={changeBg}>Change Bg</button>
+      <h6>Counter with Ref, No Re-render</h6>
+      <button onClick={() => count.current++}>{count.current}</button>
+      {console.log(count.current)}
+      <h6>Counter with State</h6>
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+      <button onClick={() => setLike(!like)}>{like ? "👍🏻" : "👎🏻"}</button>
     </div>
   );
 }
 
 export default App;
+
+// Notes
+//
