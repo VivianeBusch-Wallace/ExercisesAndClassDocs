a83d33669c3bfec1fb806f03ed13f5abba5f1c1e
viviane <v.buschwallace@yahoo.com>
2021-06-21 11:38:41 +0200

    doing useRef stuff

--- Jun/21-06/useref-hook/src/App.js
+++ Jun/21-06/useref-hook/src/App.js
@@ -4,10 +4,13 @@ function App() {
   const [name, setName] = useState("");
   const renderCount = useRef(1);
   const inputRef = useRef();
+  const prevName = useRef("");
 
   useEffect(() => {
-    renderCount.current = renderCount.current + 1;
-  });
+    // renderCount.current = renderCount.current + 1;
+    prevName.current = name;
+  }, [name]);
+
   // useState rerenders every time state changes
   // useRef does not rerender every time, but it stores the information, however it does not notify anyone about it
 
@@ -30,15 +33,23 @@ function App() {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <p>The name is {name}</p>
+      <p>
+        The name is {name}and before it was {prevName.current}
+      </p>
+
       <p>Rendering {renderCount.current} times.</p>
+
       {/* from Hadi */}
       <input type="text" ref={userTextInput} />
+
       <button onClick={getMe}>Get me focused</button>
+
       <button onClick={changeBg}>Change Bg</button>
+
       <h6>Counter with Ref, No Re-render</h6>
       <button onClick={() => count.current++}>{count.current}</button>
       {console.log(count.current)}
+
       <h6>Counter with State</h6>
       <button onClick={() => setCounter(counter + 1)}>{counter}</button>
       <button onClick={() => setLike(!like)}>{like ? "👍🏻" : "👎🏻"}</button>
