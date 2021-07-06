ea423ab6751e12ce1a7770d361d697d12dcca242
viviane <v.buschwallace@yahoo.com>
2021-07-06 10:48:07 +0200

    created everything, imported component into app, created data.json

---
+++ Jul/06-07/todo-list-class/src/App.js
@@ -0,0 +1,4 @@
+// import React from "react"; // when you don't import Component, then you need React.Component in the class component
+import React, { Component } from "react";
+
+export default class App extends Component {}
