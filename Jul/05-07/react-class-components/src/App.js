0aa1a8c86ba7ac9893371c424a15c4281602d1ce
viviane <v.buschwallace@yahoo.com>
2021-07-06 10:48:37 +0200

    added stuff from class

--- Jul/05-07/react-class-components/src/App.js
+++ Jul/05-07/react-class-components/src/App.js
@@ -1,32 +1,41 @@
 import React from "react";
 import { add, display } from "./helpers";
 
-// you can export the element directly >>
-export default class App extends React.Component {
+// you can also export the element directly >>
+// export default class App extends React.Component {}
+class App extends React.Component {
   constructor(props) {
+    // super makes it functional, without nothing in browser
     super(props);
     this.state = {
       arr: [],
-      isLogged: false,
+      isLoggedIn: false,
       data: { name: "Hadi", age: 31 },
     };
+    this.sum = function () {
+      console.log("Hi, I am a function built in const");
+    };
+    // this.sum=()=>{}
     this.handleSub = this.handleSub.bind(this);
   }
-  // Es6 Function
-  print = () => console.log("Hi I am Es6 function and I don't need to be bind");
-  // Function need to be bind
+  // Es6 Function, does not need to be bound, is already bound under the hood
+  print = () =>
+    console.log("Hi I am Es6 function and I don't need to be bound");
+  // old Function needs to be bound (line 18)
   handleSub() {
-    console.log("Hi I need to be bind");
+    console.log("Hi I need to be bound");
   }
 
   render() {
+    // console.log cannot be outside a method
     this.newUser = () => console.log("Hi I am inside render ");
 
-    this.newUser();
+    this.newUser(); //what is this for?
 
     return (
       <div>
         {this.print()}
+        {this.sum()}
         {this.handleSub()}
         {display("Hi, I like React")}
         <h1>
@@ -36,3 +45,5 @@ export default class App extends React.Component {
     );
   }
 }
+
+export default App;
