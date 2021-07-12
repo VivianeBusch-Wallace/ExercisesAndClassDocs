1883e1717b26985b7fb2395829e4f42d124e0d47
viviane <v.buschwallace@yahoo.com>
2021-07-12 09:51:55 +0200

    working on index.js step 3, 4, 5

--- Jul/12-07/redux-app/src/index.js
+++ Jul/12-07/redux-app/src/index.js
@@ -1,8 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./sass/main.scss";
+import App from "./App";
+// step 3
+import {createStore} from redux;
+import { Provider } from "react-redux";
+import allReducers from "./reducers";
+
+// step 4
+const store = createStore (
+  allReducers,
+  {
+    counter:0,
+    userName: "Hadi",
+  },
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+  )
+
+  // step 5
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+
 
 // npm i react-redux
-// const store {}
+// to install redux, then create redux files: actions, reducers
 
 // old way:
 // import React from "react";
