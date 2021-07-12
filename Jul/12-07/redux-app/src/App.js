1c5130868b023ed6965966d9e8c53566bcdc48ea
viviane <v.buschwallace@yahoo.com>
2021-07-12 10:02:12 +0200

    working on parentComponent and App.js

--- Jul/12-07/redux-app/src/App.js
+++ Jul/12-07/redux-app/src/App.js
@@ -1,3 +1,5 @@
+import React, { Fragment } from "react";
+
 function App() {
   return <div className="App"></div>;
 }
---
+++ Jul/12-07/redux-app/src/components/ParentComponent.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+// step 6
+import { connect } from "react-redux";
+
+const ParentComponent = (props) => {
+  return (
+    <Fragment>
+      <h1>ParentComponent</h1>
+    </Fragment>
+  );
+};
+
+// export default ParentComponent;
