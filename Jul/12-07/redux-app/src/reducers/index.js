379085f92fdad60db4f361847a7ec24897565cd6
viviane <v.buschwallace@yahoo.com>
2021-07-12 09:37:00 +0200

    working on reducers

---
+++ Jul/12-07/redux-app/src/reducers/index.js
@@ -0,0 +1,27 @@
+// you can have multiple reducers or just one
+
+// step 2 - this is an all in one reducer function
+const allReducers = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case "CHANGE_NAME":
+      return {
+        ...state,
+        userName: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default allReducers;
