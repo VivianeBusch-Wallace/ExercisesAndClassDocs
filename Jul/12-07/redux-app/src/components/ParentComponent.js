0385ad8d38b7b1880dcefef7e4d0f0a5a980c7c9
viviane <v.buschwallace@yahoo.com>
2021-07-12 10:11:58 +0200

    working on parentComponent

--- Jul/12-07/redux-app/src/components/ParentComponent.js
+++ Jul/12-07/redux-app/src/components/ParentComponent.js
@@ -1,13 +1,33 @@
 import React, { Fragment } from "react";
 // step 6
 import { connect } from "react-redux";
+import { changeName } from "../actions";
+import ChildComponent from "./ChildComponent";
 
 const ParentComponent = (props) => {
+  const { store, changeName } = props;
   return (
     <Fragment>
       <h1>ParentComponent</h1>
+      <button
+        onClick={() => {
+          changeName("Zain");
+        }}
+      >
+        Change Name
+      </button>
+      <ChildComponent />
     </Fragment>
   );
 };
 
+const mapStateProps = (store) => {
+  return {
+    store,
+  };
+};
+
+const mapDispatchToProps = { changeName };
+
 // export default ParentComponent;
+export default connect(mapStateProps, mapDispatchToProps)(ParentComponent);
