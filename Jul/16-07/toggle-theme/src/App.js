bc3f7ed5bfbb16bcdd2321878d37fc6341ff7cce
viviane <v.buschwallace@yahoo.com>
2021-07-16 11:27:54 +0200

    created the base for toggle theme, with react, fontawesome icons, empty navbar file, deleting unecessary files

---
+++ Jul/16-07/toggle-theme/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">This is a react app.</header>
+    </div>
+  );
+}
+
+export default App;
