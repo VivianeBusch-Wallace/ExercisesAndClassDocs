af3b0e795dd7bc85443c645c96c8732950de712b
viviane <v.buschwallace@yahoo.com>
2021-07-16 14:32:56 +0200

    nav hover and other stuff

--- Jul/16-07/toggle-theme/src/App.js
+++ Jul/16-07/toggle-theme/src/App.js
@@ -5,69 +5,73 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      <header className="App-header">This is a react app.</header>
-      <main>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-          mollis aliquam ut porttitor leo. Gravida arcu ac tortor dignissim
-          convallis aenean. Scelerisque eleifend donec pretium vulputate sapien
-          nec. Sagittis id consectetur purus ut faucibus pulvinar. Viverra
-          mauris in aliquam sem fringilla ut morbi tincidunt augue. Nisi quis
-          eleifend quam adipiscing vitae. Augue eget arcu dictum varius duis.
-          Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt
-          tortor aliquam. Mattis pellentesque id nibh tortor id aliquet lectus.
-          Habitant morbi tristique senectus et netus et malesuada. Lorem ipsum
-          dolor sit amet consectetur adipiscing. Feugiat vivamus at augue eget
-          arcu dictum. Dignissim sodales ut eu sem integer vitae justo. Nibh
-          tellus molestie nunc non blandit massa enim nec. Vitae ultricies leo
-          integer malesuada nunc vel risus. A arcu cursus vitae congue mauris
-          rhoncus. Vel facilisis volutpat est velit egestas. Laoreet non
-          curabitur gravida arcu ac tortor dignissim convallis. Nulla aliquet
-          porttitor lacus luctus accumsan. Orci dapibus ultrices in iaculis nunc
-          sed augue lacus. Est ultricies integer quis auctor elit sed. Id
-          consectetur purus ut faucibus pulvinar elementum. Condimentum vitae
-          sapien pellentesque habitant morbi tristique. Et malesuada fames ac
-          turpis egestas. Vulputate odio ut enim blandit volutpat maecenas
-          volutpat blandit. Sed euismod nisi porta lorem mollis aliquam.
-          Consectetur libero id faucibus nisl tincidunt eget nullam. Cras semper
-          auctor neque vitae tempus quam pellentesque nec. Curabitur gravida
-          arcu ac tortor dignissim convallis aenean et tortor. Arcu non odio
-          euismod lacinia at quis risus sed. Diam phasellus vestibulum lorem sed
-          risus ultricies tristique nulla. Mi in nulla posuere sollicitudin
-          aliquam ultrices sagittis.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-          mollis aliquam ut porttitor leo. Gravida arcu ac tortor dignissim
-          convallis aenean. Scelerisque eleifend donec pretium vulputate sapien
-          nec. Sagittis id consectetur purus ut faucibus pulvinar. Viverra
-          mauris in aliquam sem fringilla ut morbi tincidunt augue. Nisi quis
-          eleifend quam adipiscing vitae. Augue eget arcu dictum varius duis.
-          Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt
-          tortor aliquam. Mattis pellentesque id nibh tortor id aliquet lectus.
-          Habitant morbi tristique senectus et netus et malesuada. Lorem ipsum
-          dolor sit amet consectetur adipiscing. Feugiat vivamus at augue eget
-          arcu dictum. Dignissim sodales ut eu sem integer vitae justo. Nibh
-          tellus molestie nunc non blandit massa enim nec. Vitae ultricies leo
-          integer malesuada nunc vel risus. A arcu cursus vitae congue mauris
-          rhoncus. Vel facilisis volutpat est velit egestas. Laoreet non
-          curabitur gravida arcu ac tortor dignissim convallis. Nulla aliquet
-          porttitor lacus luctus accumsan. Orci dapibus ultrices in iaculis nunc
-          sed augue lacus. Est ultricies integer quis auctor elit sed. Id
-          consectetur purus ut faucibus pulvinar elementum. Condimentum vitae
-          sapien pellentesque habitant morbi tristique. Et malesuada fames ac
-          turpis egestas. Vulputate odio ut enim blandit volutpat maecenas
-          volutpat blandit. Sed euismod nisi porta lorem mollis aliquam.
-          Consectetur libero id faucibus nisl tincidunt eget nullam. Cras semper
-          auctor neque vitae tempus quam pellentesque nec. Curabitur gravida
-          arcu ac tortor dignissim convallis aenean et tortor. Arcu non odio
-          euismod lacinia at quis risus sed. Diam phasellus vestibulum lorem sed
-          risus ultricies tristique nulla. Mi in nulla posuere sollicitudin
-          aliquam ultrices sagittis.
-        </p>
-      </main>
+      <div className="big-container">
+        <header className="App-header">This is a react app.</header>
+        <main>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            mollis aliquam ut porttitor leo. Gravida arcu ac tortor dignissim
+            convallis aenean. Scelerisque eleifend donec pretium vulputate
+            sapien nec. Sagittis id consectetur purus ut faucibus pulvinar.
+            Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue.
+            Nisi quis eleifend quam adipiscing vitae. Augue eget arcu dictum
+            varius duis. Phasellus egestas tellus rutrum tellus pellentesque eu
+            tincidunt tortor aliquam. Mattis pellentesque id nibh tortor id
+            aliquet lectus. Habitant morbi tristique senectus et netus et
+            malesuada. Lorem ipsum dolor sit amet consectetur adipiscing.
+            Feugiat vivamus at augue eget arcu dictum. Dignissim sodales ut eu
+            sem integer vitae justo. Nibh tellus molestie nunc non blandit massa
+            enim nec. Vitae ultricies leo integer malesuada nunc vel risus. A
+            arcu cursus vitae congue mauris rhoncus. Vel facilisis volutpat est
+            velit egestas. Laoreet non curabitur gravida arcu ac tortor
+            dignissim convallis. Nulla aliquet porttitor lacus luctus accumsan.
+            Orci dapibus ultrices in iaculis nunc sed augue lacus. Est ultricies
+            integer quis auctor elit sed. Id consectetur purus ut faucibus
+            pulvinar elementum. Condimentum vitae sapien pellentesque habitant
+            morbi tristique. Et malesuada fames ac turpis egestas. Vulputate
+            odio ut enim blandit volutpat maecenas volutpat blandit. Sed euismod
+            nisi porta lorem mollis aliquam. Consectetur libero id faucibus nisl
+            tincidunt eget nullam. Cras semper auctor neque vitae tempus quam
+            pellentesque nec. Curabitur gravida arcu ac tortor dignissim
+            convallis aenean et tortor. Arcu non odio euismod lacinia at quis
+            risus sed. Diam phasellus vestibulum lorem sed risus ultricies
+            tristique nulla. Mi in nulla posuere sollicitudin aliquam ultrices
+            sagittis.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            mollis aliquam ut porttitor leo. Gravida arcu ac tortor dignissim
+            convallis aenean. Scelerisque eleifend donec pretium vulputate
+            sapien nec. Sagittis id consectetur purus ut faucibus pulvinar.
+            Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue.
+            Nisi quis eleifend quam adipiscing vitae. Augue eget arcu dictum
+            varius duis. Phasellus egestas tellus rutrum tellus pellentesque eu
+            tincidunt tortor aliquam. Mattis pellentesque id nibh tortor id
+            aliquet lectus. Habitant morbi tristique senectus et netus et
+            malesuada. Lorem ipsum dolor sit amet consectetur adipiscing.
+            Feugiat vivamus at augue eget arcu dictum. Dignissim sodales ut eu
+            sem integer vitae justo. Nibh tellus molestie nunc non blandit massa
+            enim nec. Vitae ultricies leo integer malesuada nunc vel risus. A
+            arcu cursus vitae congue mauris rhoncus. Vel facilisis volutpat est
+            velit egestas. Laoreet non curabitur gravida arcu ac tortor
+            dignissim convallis. Nulla aliquet porttitor lacus luctus accumsan.
+            Orci dapibus ultrices in iaculis nunc sed augue lacus. Est ultricies
+            integer quis auctor elit sed. Id consectetur purus ut faucibus
+            pulvinar elementum. Condimentum vitae sapien pellentesque habitant
+            morbi tristique. Et malesuada fames ac turpis egestas. Vulputate
+            odio ut enim blandit volutpat maecenas volutpat blandit. Sed euismod
+            nisi porta lorem mollis aliquam. Consectetur libero id faucibus nisl
+            tincidunt eget nullam. Cras semper auctor neque vitae tempus quam
+            pellentesque nec. Curabitur gravida arcu ac tortor dignissim
+            convallis aenean et tortor. Arcu non odio euismod lacinia at quis
+            risus sed. Diam phasellus vestibulum lorem sed risus ultricies
+            tristique nulla. Mi in nulla posuere sollicitudin aliquam ultrices
+            sagittis.
+          </p>
+        </main>
+      </div>
     </div>
   );
 }
--- Jul/16-07/toggle-theme/src/component/Navbar.js
+++ Jul/16-07/toggle-theme/src/component/Navbar.js
@@ -1,8 +1,27 @@
 import React from "react";
 // import "./App.css"; // do I need to import it here, too? I forgot
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = () => {
-  return <nav>navigation</nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#something">Something</a>
+        </li>
+      </ul>
+      <div>
+        <FontAwesomeIcon icon={faSun} />
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
