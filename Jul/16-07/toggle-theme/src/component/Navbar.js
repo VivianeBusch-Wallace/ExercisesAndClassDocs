9a1e2b1c9faeb25dd639f10e60908dfc5c3d96ed
viviane <v.buschwallace@yahoo.com>
2021-07-16 11:44:16 +0200

    added paragraphs and edited navbar and fixed fussing of react

---
+++ Jul/16-07/toggle-theme/src/component/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import "./App.css"; // do I need to import it here, too? I forgot
+
+const Navigation = () => {
+  return <nav>navigation</nav>;
+};
+
+export default Navigation;
