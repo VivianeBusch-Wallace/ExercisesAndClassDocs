0d139d38299c675d5fe59fcd351a0ff400b1c5a8
viviane <v.buschwallace@yahoo.com>
2021-08-04 09:35:14 +0200

    creating first node app, axios, nodemon, apikey for weatherapp,  etc

---
+++ Aug/04-08-21/src/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// this is not a comment, it is a command line for shebang
+// #! for triggering, then the path to the source of the env and telling it what type of application it is
+// this has to be the very first line
+// what is shebang: https://en.wikipedia.org/wiki/Shebang_(Unix)
---
+++ Aug/04-08-21/src/lib/weather-client.js
@@ -0,0 +1,9 @@
+const axios = require("axios");
+const url = require("url");
+
+class OpenWeatherClient {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseURL = "fromthewebsite";
+  }
+}
