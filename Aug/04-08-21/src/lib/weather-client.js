9f7315c8c1e341c4c2d7991ac90938333aab22e6
viviane <v.buschwallace@yahoo.com>
2021-08-04 09:52:34 +0200

    url, endpoint. etc

--- Aug/04-08-21/src/lib/weather-client.js
+++ Aug/04-08-21/src/lib/weather-client.js
@@ -6,4 +6,29 @@ class OpenWeatherClient {
     this.apiKey = apiKey;
     this.baseURL = "fromthewebsite";
   }
+  get(endpoint) {
+    const URL =
+      url.resolve(this.baseURL, endpoint) +
+      `&units=metric&appid=${this.apiKey}`;
+    return axios
+      .get(URL)
+      .then((res) => res.data)
+      .catch((err) => Promise.reject(err.response.data.message));
+  }
+  async getWeather(city, country) {
+    let endpoint = `weather?q=${city}`;
+    if (country) {
+      endpoint += `,${country}`;
+    }
+    return await this.get(endpoint);
+  }
 }
+
+// if you want to use asynchronous then use async, for synchronous use await
+
+// endpoint is for example city and country of url
+// http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${this.apiKey}
+
+// little other thing: how to convert between F° and C°
+// °C to °F: Divide by 5, then multiply by 9, then add 32°,
+// F to °C: Deduct 32, then multiply by 5, then divide by 9
