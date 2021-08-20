63593895b910e6491c5acb53916671961e63825f
viviane <v.buschwallace@yahoo.com>
2021-08-20 09:31:47 +0200

    changes for middleware

--- Aug/16-08-21/app.js
+++ Aug/16-08-21/app.js
@@ -21,34 +21,63 @@ app.use(morgan("dev")); // use morgan, check morgan responses in the console
 // MIDDLEWARE >>
 // specific route's middlewares are in their folder
 // if you want specific middleware here then make sure it is located above the use routes
-// app.use((req, res, next) => {
-//   if (!req.query.userName)
-//     return next(errorCreator(401, "Please, login to see this page.")); // if the user is not logged in, then they cannot see the page and will get an error message
-//     next()
-// });
-
+app.use("/admin", (req, res, next) => {
+  if (!req.query.userName)
+    return next(createError(401, "Please login to see this page.")); // if the user is not logged in, then they cannot see the page and will get an error message
+  next();
+});
 // Uses - Assigning Routes >>
-// app.use("/users", userRouter);
-// app.use("/books", bookRouter);
+app.use("/users", userRouter);
+app.use("/books", bookRouter);
+
+// any error might happen
+// app.use((req, res, next) => {
+//   const error = new Error("Noooooo this is not cool");
+//   error.status = 404;
+//   next(error);
+// });
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message,
+//     },
+//   });
+// });
 
 // ROOT ROUTE (main route)
 app
   .get("/", (req, res) => {
-    console.log("GET");
+    console.log("get req");
     res.status(200).json({ text: "This is a GET req." });
   })
   .post("/", (req, res) => {
-    console.log("POST");
+    console.log("post req");
     res.status(200).json({ text: "This is a POST req" });
   })
   .put("/", (req, res) => {
-    console.log("PUT");
+    console.log("put req");
     res.status(200).json({ text: "This is a PUT req" });
   })
   .delete("/", (req, res) => {
-    console.log("DELETE");
+    console.log("delete req");
     res.status(200).json({ text: "This is a DELETE req" });
   });
+
+app.post("/bill", (req, res, next) => {
+  // if
+  next(
+    errorCreator(402, `You don't have enough 💶`, {
+      detail: {
+        yourBalance: 400,
+        itemCost: 6000,
+      },
+    })
+  ); // PaymentRequiredError: The HTTP 402 Payment Required is a nonstandard client error status response code that is reserved for future use. Sometimes, this code indicates that the request can not be processed until the client makes a payment.
+  res.send("this is bill page");
+});
+
+module.exports = app;
 
 // Notes:
 /* we will create our own apis
@@ -56,4 +85,5 @@ we don't hide our PORT for security reasons, it just is in the config file and w
 If we want to create a secure connection with https then do we write:
 const server = https.createServer(app);  ? and what if we want both to work? or maybe if someone types http... we automatically turn it into https, how does that work?
 >> No http is for our build, if you want to add security level on your app you can use the domain (the one that host your app) to secure your app
+Never forget to export app or the other routers
 */
